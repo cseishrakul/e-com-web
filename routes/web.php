619650c101f66573f3fd8c70e8ca526048d1d9ad
Efaz <ishrakul236@gmail.com>
2024-07-04 23:47:04 +0600

    E-commerce project

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth', 'role:user'])->group(function () {
+    Route::get('/admin/dashboard', [DashboardController::class, 'index']);
+});
+
+
+require __DIR__ . '/auth.php';
